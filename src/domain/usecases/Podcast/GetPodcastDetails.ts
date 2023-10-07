25707822d1fbabb09167b3867ce46c80cd43a15f
Rubem <rubemfsv15@gmail.com>
2023-10-07 11:32:17 -0300

    refactor: remove unused params from getDetails

--- src/domain/usecases/Podcast/GetPodcastDetails.ts
+++ src/domain/usecases/Podcast/GetPodcastDetails.ts
@@ -1,14 +1,9 @@
 import { PodcastDetailsRequestModel } from '@/domain/models'
 
 export interface IGetPodcastDetails {
-  getDetails: (
-    params: GetPodcastDetails.Params
-  ) => Promise<GetPodcastDetails.Model>
+  getDetails: () => Promise<GetPodcastDetails.Model>
 }
 
 export namespace GetPodcastDetails {
-  export type Params = {
-    id: number
-  }
   export type Model = PodcastDetailsRequestModel
 }
