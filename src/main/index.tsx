edc9c088f4d21f9bee14799cf2197a95fc1fbd8e
Rubem <rubemfsv15@gmail.com>
2023-10-07 09:10:05 -0300

    feat: add router to index

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Router from '@/main/routes/router'
 
-const rootElement = document.getElementById('main') as Element
+const rootElement = document.getElementById('main')
 const root = createRoot(rootElement)
 
 root.render(
   <StrictMode>
-    <h1>Hello World</h1>
+    <Router />
   </StrictMode>
 )
