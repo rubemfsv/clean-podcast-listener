26b7c1cbe85ae3426fbdf492d40f6b6cf4d08d53
Rubem <rubemfsv15@gmail.com>
2023-10-07 11:51:12 -0300

    chore: change type to number on usecase

--- src/main/factories/usecases/Podcast/RemoteGetPodcastDetails/index.ts
+++ src/main/factories/usecases/Podcast/RemoteGetPodcastDetails/index.ts
@@ -2,7 +2,7 @@ import { RemoteGetPodcastDetails } from '@/data/usecases/'
 import { IGetPodcastDetails } from '@/domain/usecases'
 import { makeAxiosHttpClient, makeApiUrl } from '@/main/factories/http'
 
-export const makeRemoteGetPodcastDetails = (id: string): IGetPodcastDetails => {
+export const makeRemoteGetPodcastDetails = (id: number): IGetPodcastDetails => {
   const remoteGetPodcastDetails = new RemoteGetPodcastDetails(
     makeApiUrl(`lookup?id=${id}&media=podcast &entity=podcastEpisode&limit=20`),
     makeAxiosHttpClient()
