d2e2c78682525f2f288a4637240ecc1b906199f0
Rubem <rubemfsv15@gmail.com>
2023-10-08 20:37:30 -0300

    feat: create format date helper

--- src/presentation/utils/dateTime.ts
+++ src/presentation/utils/dateTime.ts
@@ -7,7 +7,20 @@ export function isDateOneMonthFromNow(receivedDate: Date): boolean {
   return timeDifference >= oneMonthInMilliseconds
 }
 
-export function millisecondsToMinutes(milliseconds) {
+export function formatDate(inputDate: string): string {
+  const date = new Date(inputDate)
+  const day = date.getDate()
+  const month = date.getMonth() + 1
+  const year = date.getFullYear()
+
+  const formattedDate = `${String(day).padStart(2, '0')}/${String(
+    month
+  ).padStart(2, '0')}/${year}`
+
+  return formattedDate
+}
+
+export function millisecondsToMinutes(milliseconds: number): string {
   const totalSeconds = Math.floor(milliseconds / 1000)
   const minutes = Math.floor(totalSeconds / 60)
   const seconds = totalSeconds % 60
