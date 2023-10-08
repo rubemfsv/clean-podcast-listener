c479e84abfb2f935d5b4303375d310af520edcd5
Rubem <rubemfsv15@gmail.com>
2023-10-08 20:29:03 -0300

    feat: create millisecondsToMinutes helper

--- src/presentation/utils/dateTime.ts
+++ src/presentation/utils/dateTime.ts
@@ -6,3 +6,15 @@ export function isDateOneMonthFromNow(receivedDate: Date): boolean {
   const oneMonthInMilliseconds = 30 * 24 * 60 * 60 * 1000
   return timeDifference >= oneMonthInMilliseconds
 }
+
+export function millisecondsToMinutes(milliseconds) {
+  const totalSeconds = Math.floor(milliseconds / 1000)
+  const minutes = Math.floor(totalSeconds / 60)
+  const seconds = totalSeconds % 60
+
+  const formattedTime = `${String(minutes).padStart(2, '0')}:${String(
+    seconds
+  ).padStart(2, '0')}`
+
+  return formattedTime
+}
